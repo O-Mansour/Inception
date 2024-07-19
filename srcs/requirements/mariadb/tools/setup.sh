34c8aff88295c0d4f1ce42f90e1b571f79e08595
Oussama <spark.ouss@gmail.com>
2024-07-19 03:30:57 +0100

    Small changes (in the order), v1 is ready

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -4,7 +4,7 @@ sed -i "s/bind-address            = 127.0.0.1/bind-address = 0.0.0.0/g" /etc/mys
 
 service mariadb start
 
-sleep 3
+sleep 2
 
 mariadb -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
 mariadb -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_USER_PWD';"
--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar && \
+    chmod +x wp-cli.phar && \
+    mv wp-cli.phar /usr/local/bin/wp
+
 wp core download --allow-root
 
 wp config create --allow-root \
